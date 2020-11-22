6b9c9cc19af396ada471ac3c9eaec1e9ba947723
Guimauve <guillaume.bournel@gmail.com>
2020-11-22 14:21:08 +0100

    doing ex4

--- 04/higherOrder.hs
+++ 04/higherOrder.hs
@@ -10,6 +10,7 @@ fun1 (x:xs)
 
 fun1' :: [Integer] -> Integer
 fun1' = product . map (\x -> x - 2) . filter (even)
+-- fun1' = product . map (subtract 2) . filter (even)
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -17,4 +18,15 @@ fun2 n | even n     = n + fun2 (n `div` 2)
        | otherwise  = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' = undefined
+fun2' = sum 
+      . filter even     
+      . takeWhile (/= 1)
+      . iterate (\n -> if even n then (n `div` 2) else (3 * n + 1)) 
+
+-- Exercice 2: Folding with trees
+-- Each node stores an extra Integer representing the height at that node
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+
